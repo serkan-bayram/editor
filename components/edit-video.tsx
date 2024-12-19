76d24ae5461c382ba3147a2b7b67febe9863e8ab
Serkan Bayram <serkanbay02@hotmail.com>
2024-12-19 02:18:00 +0100

    Ability to show seconds instead of frames

--- components/edit-video.tsx
+++ components/edit-video.tsx
@@ -3,11 +3,10 @@
 import Image from "next/image";
 import { Dispatch, memo, SetStateAction, useState } from "react";
 import { Frame } from "./frame";
-import { Button } from "./ui/button";
-import { useAppDispatch } from "@/lib/hooks";
-import { addText } from "@/lib/features/frame/frameSlice";
-import { makeVideo } from "@/app/actions";
 import { EditorBar } from "./editor-bar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const FPS_RATE = 30;
 
 export function EditVideo({
   frameCount,
@@ -43,24 +42,56 @@ export const Timeline = memo(function Timeline({
   setSelectedFrame: Dispatch<SetStateAction<number>>;
 }) {
   return (
-    <div className="p-2 border flex gap-x-4 overflow-x-scroll">
-      {Array.from({ length: frameCount }).map((_, index) => (
-        <button
-          className="flex-shrink-0 rounded-md hover:opacity-75 transition-opacity"
-          key={index}
-          onClick={() => setSelectedFrame(index + 1)}
-        >
-          <Image
-            loading="lazy"
-            className="rounded-md"
-            alt={`Frame ${index + 1}`}
-            src={`/api/frames/${videoId}/${index + 1}`}
-            width={192}
-            height={96}
-          />
-          Frame {index + 1}
-        </button>
-      ))}
-    </div>
+    <Tabs defaultValue="frames">
+      <TabsList>
+        <TabsTrigger value="frames">frames</TabsTrigger>
+        <TabsTrigger value="seconds">seconds</TabsTrigger>
+      </TabsList>
+      <TabsContent value="frames">
+        <div className="w-full p-2 border flex gap-x-4 overflow-x-scroll">
+          {Array.from({ length: frameCount }).map((_, index) => (
+            <button
+              className="flex-shrink-0 rounded-md hover:opacity-75 transition-opacity"
+              key={index}
+              onClick={() => setSelectedFrame(index + 1)}
+            >
+              <Image
+                loading="lazy"
+                className="rounded-md"
+                alt={`Frame ${index + 1}`}
+                src={`/api/frames/${videoId}/${index + 1}`}
+                width={192}
+                height={96}
+              />
+              Frame {index + 1}
+            </button>
+          ))}
+        </div>
+      </TabsContent>
+      <TabsContent value="seconds">
+        <div className="w-full p-2 border flex gap-x-4 overflow-x-scroll">
+          {Array.from({ length: frameCount }).map((_, index) => {
+            if (index % FPS_RATE !== 0) return;
+            return (
+              <button
+                className="flex-shrink-0 rounded-md hover:opacity-75 transition-opacity"
+                key={index}
+                onClick={() => setSelectedFrame(index + 1)}
+              >
+                <Image
+                  loading="lazy"
+                  className="rounded-md"
+                  alt={`Frame ${index + 1}`}
+                  src={`/api/frames/${videoId}/${index + 1}`}
+                  width={192}
+                  height={96}
+                />
+                {index / FPS_RATE + 1}. Second
+              </button>
+            );
+          })}
+        </div>
+      </TabsContent>
+    </Tabs>
   );
 });
