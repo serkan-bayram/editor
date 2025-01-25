be3f54710e3dddddf77bcc0d473d2da8b4820561
Serkan Bayram <serkanbay02@hotmail.com>
2025-01-25 02:47:36 +0100

    Add more hotkeys

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -3,7 +3,11 @@ import { EditText } from "./ui/edit-text";
 import { Features } from "./ui/editor-bar-features";
 import { EditImage } from "./ui/edit-image";
 import { useHotkeys } from "react-hotkeys-hook";
-import { addComponent } from "@/lib/features/video/videoSlice";
+import {
+  addComponent,
+  deleteComponent,
+  setFocus,
+} from "@/lib/features/video/videoSlice";
 
 export function SideBar() {
   const focusedComponent = useAppSelector(
@@ -12,50 +16,45 @@ export function SideBar() {
   const texts = useAppSelector((state) => state.video.texts);
   const images = useAppSelector((state) => state.video.images);
 
+  const wholeArray = [...texts, ...images];
+
   const dispatch = useAppDispatch();
 
   useHotkeys(
-    ["ctrl+v"],
-    () => {
-      if (!focusedComponent) return;
-      console.log(focusedComponent);
+    ["ctrl+v, delete", "escape"],
+    (_, handler) => {
+      const component = wholeArray.find(
+        (comp) => comp.id === focusedComponent?.id
+      );
 
-      switch (focusedComponent.component) {
-        case "image":
-          const image = images.find(
-            (image) => image.id === focusedComponent.id
-          );
-          if (!image) return;
+      if (!component) return;
+
+      switch (handler.keys?.join("")) {
+        case "v":
+          const id = window.crypto.randomUUID();
           dispatch(
             addComponent({
-              ...image,
-              id: window.crypto.randomUUID(),
+              ...component,
+              id: id,
               secondsRange: {
-                start: image.secondsRange.end,
-                end: image.secondsRange.end + 0.1,
+                start: component.secondsRange.end,
+                end: component.secondsRange.end + 0.1,
               },
             })
           );
+          dispatch(setFocus({ component: component.type, id: id }));
           break;
-        case "text":
-          const text = texts.find((text) => text.id === focusedComponent.id);
-          if (!text) return;
-          dispatch(
-            addComponent({
-              ...text,
-              id: window.crypto.randomUUID(),
-              secondsRange: {
-                start: text.secondsRange.end,
-                end: text.secondsRange.end + 0.1,
-              },
-            })
-          );
+        case "delete":
+          dispatch(deleteComponent(component));
+          break;
+        case "escape":
+          dispatch(setFocus(undefined));
           break;
         default:
           break;
       }
     },
-    [focusedComponent]
+    [focusedComponent, wholeArray]
   );
 
   return (
